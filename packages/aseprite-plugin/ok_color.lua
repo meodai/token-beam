057bf81f8cdd10ad12e76993157183fcb4a7ee06
David Aerne <meodai@gmail.com>
2026-03-10 22:35:40 +0100

    chore(ok_color): update copyright information and license details

--- packages/aseprite-plugin/ok_color.lua
+++ packages/aseprite-plugin/ok_color.lua
@@ -1,6 +1,37 @@
 -- Adapted from https://github.com/behreajj/AsepriteOkHsl/blob/main/ok_color.lua
--- Copyright (c) 2021 Bjorn Ottosson
--- MIT License
+-- Original Oklab code and license reference:
+-- https://bottosson.github.io/posts/oklab/
+-- https://bottosson.github.io/misc/License.txt
+--
+-- Copyright (c) 2020 Bjorn Ottosson
+--
+-- Permission is hereby granted, free of charge,
+-- to any person obtaining a copy of
+-- this software and associated documentation
+-- files (the "Software"), to deal in
+-- the Software without restriction, including
+-- without limitation the rights to
+-- use, copy, modify, merge, publish, distribute,
+-- sublicense, and/or sell copies
+-- of the Software, and to permit persons to whom the
+-- Software is furnished to do
+-- so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT
+-- WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+-- WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+-- NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+-- OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+-- THE SOFTWARE.
 
 local ok_color = {}
 
